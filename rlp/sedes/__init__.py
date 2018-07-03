d7702da1f9beebfef611324a5d5a702e2909fd25
Phuc Nguyen <phucnguyen.it@gmail.com>
2018-07-04 00:38:06 +0700

    adding text sedes

--- rlp/sedes/__init__.py
+++ rlp/sedes/__init__.py
@@ -2,3 +2,4 @@ from . import raw
 from .binary import Binary, binary
 from .big_endian_int import BigEndianInt, big_endian_int
 from .lists import CountableList, List, Serializable, make_immutable, make_mutable
+from .text import text
