40726e2f8a72e066c9da36aa59bf6bf1a8770259
Phuc Nguyen <phucnguyen.it@gmail.com>
2018-07-04 13:43:42 +0700

    Adding ALL_BYTES to utils

--- rlp/utils.py
+++ rlp/utils.py
@@ -1,5 +1,12 @@
 import sys
+import struct
+
 if sys.version_info.major == 2:
     from .utils_py2 import *
 else:
     from .utils_py3 import *
+
+ALL_BYTES = tuple(
+    struct.pack('B', i)
+    for i in range(256)
+)
